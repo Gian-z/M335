da2b42406dff2df31610c608ecdbdeceadc7dbca
GianLuca <gianluca.zwahlen@cmiag.ch>
2024-01-11 16:35:12 +0100

    bmi drop

---
+++ BMI/app/src/main/java/com/gzw/bmi/CalculationResultActivity.java
@@ -0,0 +1,14 @@
+package com.gzw.bmi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalculationResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
